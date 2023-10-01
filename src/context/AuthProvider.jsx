26064dd97f97544e44d843fd08949b215a55f522
axeladib <nabil298adib@gmail.com>
2023-10-01 09:16:28 +0800

    Authentication : AuthProvider.jsx : Add the getSession function and store it tinside the session and make it available in all of the application

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -18,25 +18,35 @@ const SignIn = (email, password) => {
 };
 const AuthProvider = ({ children }) => {
   //TODO: Create the state management for user and auth
-  const [user,setUser] = useState(null);
-  const [auth,setAuth] = useState(false);
+  const [user, setUser] = useState(null);
+  const [auth, setAuth] = useState(false);
+  const [session, setSession] = useState(null);
 
   //TODO: Setup the user session and user login with onAuthStateChange that will return event, session
-  useEffect(()=>{
-    const {data} = supabase.auth.onAuthStateChange((event,session)=>{
-        if(event == "SIGNED_UP"){
-            setUser(session.user);
-            setAuth(true);
-        }
-    })
+  useEffect(() => {
+    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event == "SIGNED_UP") {
+        setUser(session.user);
+        setAuth(true);
+      }
+    });
+    //TODO: Get the user session
+    //FIXME : Vulnerable for error
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
     //TODO : Create the unsubscribe of user to prevent the memory leakage
-    return ()=>{
-       data.subscription.unsubscribe();
-    }
-  },[])
-  
+    return () => {
+      data.subscription.unsubscribe();
+    };
+  }, []);
+
   //TODO: AuthContext.Provider make the value is available for all the component but just providing while useContext make we can manupulate the value
-  return <AuthContext.Provider value={{user, SignIn}}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ user, SignIn, session }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
